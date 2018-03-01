b67fe494279482f1971d3bcc31bef019145e9a51
crx <940909850@qq.com>
2018-03-01 09:14:50 +0800

    暂存Update

--- Update/Update.py
+++ Update/Update.py
@@ -30,11 +30,9 @@ class Ipset():
     Ture is or not useful
     """
     def __init__(self,ip):
-        # self.ipset = [[i, True] for i in ip]
         self.ipset = ip
-        self.lock = threading.Lock()
+        self.lock = threading.RLock()
         self.size = len(ip)
-        # self.OriginSize = self.size
         self.index = -1
     def empty(self):
         return self.size < 1
@@ -74,31 +72,28 @@ class Update(object):
         self.parseIndex = parseIndex
         self.parsePage = parsePage
         self.NumThread = NumThread
-        self.IsOver = False
         self.MailMsg = '<h2>{site}</h2>'.format(site=siteName)
+        self.signal = threading.Event()
         self.resultlist = []
         self.firstlink = queue.Queue()
 
-    def download(self, url, cycle=5):
-        # cycle = cy
+    def download(self, url, cy=5):
+        cycle = cy
         global User_agent_list,ipset
         while cycle > 0:
             proxy = ipset.getip()
             if proxy is None:
                 return None
-
             proxies = {'http':'http://'+proxy,'https':'https://'+proxy}
             headers = {'User-Agent':random.choice(User_agent_list)}
             try:
-                # rootLogger.critical("downloading "+url)
-                # rootLogger.critical(threading.current_thread().getName() + " trying num " + str(6-cycle) + " times via " + proxy)
-                r = requests.get(url, headers=headers,proxies=proxies,timeout=2)
-                ipset.releaseip(proxy)
+                r = requests.get(url, headers=headers,proxies=proxies,timeout=1)
                 if r.status_code == 200:
                     break
             except Exception:
                 cycle -= 1
                 time.sleep(2)
+            finally:
                 ipset.releaseip(proxy)
 
         if cycle > 0 and len(r.text) > 100:
@@ -120,11 +115,9 @@ class Update(object):
 
     def getsource(self):
         while not self.firstlink.empty():
-            # rootLogger.critical(threading.current_thread().getName(
-            # ) + " still working" + " empty:" + str(self.firstlink.empty()))
             url = self.firstlink.get()
-            # rootLogger.critical(
-            #     "naw queue size: " + str(self.firstlink.qsize()))
+            self.firstlink.task_done()
+            rootLogger.error(threading.current_thread().getName()+" getsource working...")
             html = self.download(url)
             if html is None:
                 return None
@@ -137,12 +130,16 @@ class Update(object):
         # global NumOutput
         cursor = self.connection.cursor()
         # sqlLock = threading.Lock()
+        UpdateNum = 0
+        item_id = 0
         NewSourceDate = time.strftime("%Y-%m-%d")
         while (len(self.resultlist) > 0) or (self.NumThread > 0):
+            rootLogger.error(threading.current_thread().getName()+" output working...")
             if len(self.resultlist) > 0:
                 item = self.resultlist.pop()
                 if item[4]:
                     # sqlLock.acquire()
+                    # item_id = 0
                     ObjNum = cursor.execute(
                         "select id from movie_items where name = '{name}'".
                         format(name=item[0]))
@@ -154,45 +151,50 @@ class Update(object):
                         if LinkNum > 0:
                             LinkNum = cursor.fetchone()[0]
                             LinkSize = len(item[3])
-                            item[3] = item[3][0:(LinkSize-LinkNum)]
+                            Inc = LinkSize - LinkNum
+                            item[3] = item[3][0:Inc]
+                            UpdateNum = Inc + LinkNum
+                            UpdateNum = "更新至第" +str(UpdateNum) + "集"
+                        else:
+                            UpdateNum = "此次增加" + str(len(item[3])) + "数据"
+                    else:
+                        item_id = 0
                     # sqlLock.release()
 
                 if len(item[3]) > 0:
-                    # rootLogger.critical(item[0]+" "+item[1]+" "+item[2]+" " +str(item[3]))
                     try:
-                        cursor.execute(
-                            'insert into movie_items(name,img,tag,pubdate) values (%s,%s,%s,%s)',
-                            (item[0], item[1], item[2], NewSourceDate))
-                        id = cursor.lastrowid
+                        if item_id == 0:
+                            cursor.execute(
+                                'insert into movie_items(name,img,tag,pubdate) values (%s,%s,%s,%s)',
+                                (item[0], item[1], item[2], NewSourceDate))
+                            id = cursor.lastrowid
+                            UpdateNum = "有" + str(len(item[3])) + "条下载链接"
+                        else:
+                            id = item_id
                         info = [tuple([each, id]) for each in item[3]]
                         cursor.executemany('insert into movie_links(link,item_id) values (%s,%s)',info)
                         self.connection.commit()
-                        self.MailMsg = self.MailMsg + "<h4>{name}</h4><img src={href}>".format(
-                            name=item[0], href=item[1])
+                        self.MailMsg = self.MailMsg + "<h3>{name}</h3><p>{series}</p><img src={href}>".format(
+                            name=item[0], series=UpdateNum, href=item[1])
                     except Exception as e:
                         self.connection.rollback()
                         rootLogger.error(str(e))
-            else:
-                time.sleep(5)
-        # NumOutput = NumOutput - 1
-        self.IsOver = True
+        self.signal.set()
         rootLogger.critical(threading.current_thread().getName() + " exited")
         # rootLogger.critical(threading.current_thread().getName()+" working result:" + str(len(self.resultlist)) + " Numthread:"+str(self.NumThread)+" qsize:"+str(self.firstlink.qsize()))
-    
+
     def sendEmai(self):
-        # global NumOutput
-        time.sleep(20)
-        # rootLogger.error("NumOutput:" + str(NumOutput))
-        while not self.IsOver:
-            rootLogger.error("waiting...")
-            time.sleep(20)
-        rootLogger.error(self.MailMsg)
+        self.signal.wait()
         NewSourceDate = time.strftime("%Y-%m-%d")
         mail.mail(self.MailMsg,subject=NewSourceDate + " 电影更新情况")
 
     def run(self):
         self.getpagelinks()
-        # rootLogger.critical(str(self.firstlink))
+        if self.firstlink.empty():
+            self.signal.set()
+            self.MailMsg = self.MailMsg + "<p>今天没有资源更新</p>"
+            self.sendEmai()
+            return
         num = self.NumThread
         while num > 0:
             t = threading.Thread(target=self.getsource)
